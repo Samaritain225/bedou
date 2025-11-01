7ad631fed66365e74f30c740188c1713c96c9ecb
@Doumbia225_Sam <doumbia.tiemoko@adgroupe.io>
2025-11-01 20:28:22 +0000

    chore: Remove app.json configuration file and update eas.json and package.json for development environment setup. Modify French translations for improved clarity and consistency.

---
+++ app.config.ts
@@ -0,0 +1,83 @@
+import { ConfigContext, ExpoConfig } from 'expo/config';
+
+const isDev = process.env.APP_VARIANT === 'development';
+
+export default ({ config }: ConfigContext): ExpoConfig => {
+  return {
+    ...config,
+    name: isDev ? 'Bedou (DEV)' : 'Bedou',
+    slug: 'bedou',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/images/icon.png',
+    scheme: 'bedou',
+    userInterfaceStyle: 'automatic',
+    newArchEnabled: true,
+    ios: {
+      ...config.ios,
+      supportsTablet: true,
+      bundleIdentifier: isDev ? 'com.bedou.io.dev' : 'com.bedou.io',
+    },
+    android: {
+      ...config.android,
+      adaptiveIcon: {
+        backgroundColor: '#E6F4FE',
+        foregroundImage: './assets/images/android-icon-foreground.png',
+        backgroundImage: './assets/images/android-icon-background.png',
+        monochromeImage: './assets/images/android-icon-monochrome.png',
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      package: isDev ? 'com.bedou.io.dev' : 'com.bedou.io',
+    },
+    web: {
+      ...config.web,
+      output: 'static',
+      favicon: './assets/images/favicon.png',
+    },
+    plugins: [
+      'expo-router',
+      [
+        'expo-splash-screen',
+        {
+          image: './assets/images/splash-icon.png',
+          imageWidth: 200,
+          resizeMode: 'contain',
+          backgroundColor: '#ffffff',
+          dark: {
+            backgroundColor: '#000000',
+          },
+        },
+      ],
+      'expo-localization',
+      [
+        'expo-sqlite',
+        {
+          enableFTS: true,
+          useSQLCipher: true,
+          android: {
+            enableFTS: false,
+            useSQLCipher: false,
+          },
+          ios: {
+            customBuildFlags: [
+              '-DSQLITE_ENABLE_DBSTAT_VTAB=1 -DSQLITE_ENABLE_SNAPSHOT=1',
+            ],
+          },
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
+    extra: {
+      ...config.extra,
+      router: {},
+      eas: {
+        projectId: '16f36a08-e61f-4693-b336-29d920a146ba',
+      },
+    },
+  };
+};
+
